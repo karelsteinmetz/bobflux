76074649a0918e952239fdae0f69e4690633e647
k.steinmetz <karel.steinmetz@gmail.com>
2015-09-26 17:01:44 +0200

    Created project infrastructure.

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+﻿var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var jasmine = require('gulp-jasmine');
+
+gulp.task('default', ['tsCompilation', 'runTests']);
+
+gulp.task('tsCompilation', function () {
+    var tsProject = ts.createProject('tsconfig.json');
+    return tsProject.src()
+        .pipe(ts(tsProject)).js
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('runTests', ['tsCompilation'], function () {
+    return gulp.src('dist/spec/**/*.spec.js')
+        .pipe(jasmine({
+            verbose: true,
+            includeStackTrace: true
+        }));
+});
