8a8299a5f3583cfe82a8144eadd2b40051af69b1
k.steinmetz <karel.steinmetz@gmail.com>
2015-09-26 19:00:30 +0200

    Added gulp task (srcTsMove) to build.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@
 var ts = require('gulp-typescript');
 var jasmine = require('gulp-jasmine');
 
-gulp.task('default', ['tsCompilation', 'runTests']);
+var distDir = 'dist';
+
+gulp.task('default', ['tsCompilation', 'srcTsMove', 'runTests']);
 
 gulp.task('tsCompilation', function () {
     var tsProject = ts.createProject('tsconfig.json');
     return tsProject.src()
         .pipe(ts(tsProject)).js
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(distDir));
 });
 
 gulp.task('runTests', ['tsCompilation'], function () {
@@ -17,4 +19,9 @@ gulp.task('runTests', ['tsCompilation'], function () {
             verbose: true,
             includeStackTrace: true
         }));
+});
+
+gulp.task('srcTsMove', function () {
+    return gulp.src('src/**/*.ts')
+        .pipe(gulp.dest(distDir + '/src'));
 });
