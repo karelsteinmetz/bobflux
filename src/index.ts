d4108423b7f55b5481a660e35d645f27502236e1
k.steinmetz <karel.steinmetz@gmail.com>
2015-10-15 22:37:37 +0200

    Added reexporting for createActions from fun-model.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as b from 'node_modules/bobril/index';
 import { IState, bootstrap as funBootstrap } from 'node_modules/fun-model/dist/src/index';
 
-export { ICursor, IState, getState, setState, rootCursor, shallowCopy, IAction, createAction } from 'node_modules/fun-model/dist/src/index';
+export { ICursor, IState, getState, setState, rootCursor, shallowCopy, IAction, createAction, createActions } from 'node_modules/fun-model/dist/src/index';
 export * from './component';
 
 export let bootstrap = (defaultState: IState, subStateSeparator: string = '.') => {
