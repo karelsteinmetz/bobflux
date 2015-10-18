b4410ea139c044aac8b4f5dc037abe2e51a2babd
k.steinmetz <karel.steinmetz@gmail.com>
2015-10-18 15:33:17 +0200

    Created debugCallback.

--- examples/todo/src/bootstrap.ts
+++ examples/todo/src/bootstrap.ts
@@ -2,5 +2,5 @@ import * as bobflux from '../node_modules/bobflux/dist/src/index';
 import * as states from './states';
 
 export default () => {
-    bobflux.bootstrap(states.default());
+    bobflux.bootstrap(states.default(), (m, p) => console.log('todo -> ' + m, p));
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as b from 'node_modules/bobril/index';
-import { IState, bootstrap as funBootstrap } from 'node_modules/fun-model/dist/src/index';
+import { IState, bootstrap as funBootstrap, debugCallbackType } from 'node_modules/fun-model/dist/src/index';
 
-export { ICursor, IState, getState, setState, rootCursor, shallowCopy, IAction, createAction, createActions } from 'node_modules/fun-model/dist/src/index';
+export { ICursor, IState, getState, setState, rootCursor, shallowCopy, IAction, createAction, createActions, debugCallbackType } from 'node_modules/fun-model/dist/src/index';
 export * from './component';
 
-export let bootstrap = (defaultState: IState, subStateSeparator: string = '.') => {
-    funBootstrap(defaultState, () => b.invalidate());
+export let bootstrap = (defaultState: IState, debugCallback: debugCallbackType = undefined, subStateSeparator: string = '.') => {
+    funBootstrap(defaultState, () => b.invalidate(), (message, params) => { debugCallback && debugCallback(`bobflux -> ${message}`, params) });
 };
