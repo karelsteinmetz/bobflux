52a644ca326ee79a37bef43efb45cb80b90daca4
k.steinmetz <karel.steinmetz@gmail.com>
2016-01-12 23:42:58 +0100

    Created add new todo form.

--- examples/todo/src/actions.ts
+++ examples/todo/src/actions.ts
@@ -1,32 +1,33 @@
 import * as f from './flux';
-import { IAction } from './flux';
-import * as states from './states';
-import * as cursors from './cursors';
+import * as s from './states';
+import * as c from './cursors';
 
-export let addTodo = f.createAction(cursors.todos, (todos: states.ITodo[], name: string): states.ITodo[] => {
-    return [{ id: Date.now(), name: name, isDone: false }, ...todos];
-});
-
-export let updateEditedTodoName = f.createAction(cursors.editedTodo, (todo: states.ITodo, name: string): states.ITodo => {
-    return f.shallowCopy(todo, (t) => {
-        t.name = name;
-        return t;
-    });
-});
-
-export let removeTodo = f.createAction(
-    cursors.todos,
-    (todos: states.ITodo[], id: number): states.ITodo[] => {
-        return [...todos.filter(t => t.id !== id)];
-    }
+export let addTodo = f.createAction<s.ITodosState, any>(c.todosSection, (state) =>
+    f.shallowCopy(state, (section) => {
+        section.todos = [
+            { id: new Date().getTime(), isDone: false, name: section.editedTodo.name },
+            ...section.todos
+        ];
+        section.editedTodo = { id: null, name: '', isDone: false };
+    })
 );
+
+export let updateNewTodoName = f.createAction<s.ITodo, string>(c.editedTodo, (todo, name) =>
+    f.shallowCopy(todo, (t) => {
+        t.name = name;
+    })
+);
+
+export let removeTodo = f.createAction<s.ITodo[], number>(c.todos, (todos, id) => {
+    return [...todos.filter(t => t.id !== id)];
+});
 
 export interface IChangeCompletionParams {
     id: number;
     isDone: boolean;
 }
 
-export let changeDoneStatus = f.createAction<states.ITodo[], IChangeCompletionParams>(cursors.todos, (todos, params) => {
+export let changeDoneStatus = f.createAction<s.ITodo[], IChangeCompletionParams>(c.todos, (todos, params) => {
     return todos.map(t => {
         if (t.id === params.id)
             return f.shallowCopy(t, (nT) => {
--- examples/todo/src/todosPage.ts
+++ examples/todo/src/todosPage.ts
@@ -3,13 +3,34 @@ import * as gui from 'bobril-css-bootstrap';
 import * as f from './flux';
 import * as s from './states';
 import * as a from './actions';
+import * as c from './cursors';
 
 
 export let create = f.createRouteComponent<s.ITodosState, any>({
     render(ctx: f.IContext<s.ITodosState>, me: b.IBobrilNode, oldMe?: b.IBobrilCacheNode) {
+        me.children = [
+            addForm(c.editedTodo),
+            table(c.todos)
+        ]
+    }
+})
+
+let addForm = f.createComponent<s.ITodo, any>({
+    render(ctx: f.IContext<s.ITodo>, me: b.IBobrilNode, oldMe?: b.IBobrilCacheNode) {
+        me.children = gui.inlineForm({
+            content: [
+                gui.inputFormField('', ctx.state.name, a.updateNewTodoName),
+                gui.button({ label: 'Add', onClick: () => { a.addTodo(); return true; } })
+            ]
+        })
+    }
+})
+
+let table = f.createComponent<s.ITodo[], any>({
+    render(ctx: f.IContext<s.ITodo[]>, me: b.IBobrilNode, oldMe?: b.IBobrilCacheNode) {
         me.children = gui.table({
             headers: [],
-            rows: ctx.state.todos.map(t => [gui.checkboxInput({ value: t.isDone, onChange: (v) => a.changeDoneStatus({ id: t.id, isDone: v }) }), t.name])
+            rows: ctx.state.map(t => [gui.checkboxInput({ value: t.isDone, onChange: (v) => a.changeDoneStatus({ id: t.id, isDone: v }) }), t.name])
         });
     }
 })
