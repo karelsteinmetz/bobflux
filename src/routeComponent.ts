d3fb6b404d657f1764f78bb17df2b308474b4635
k.steinmetz <karel.steinmetz@gmail.com>
2016-01-18 08:17:11 +0100

    Fixed rendering issues in routeComponent on data change.

--- src/routeComponent.ts
+++ src/routeComponent.ts
@@ -2,12 +2,34 @@ import * as b from 'bobril';
 import * as f from 'fun-model';
 import * as cm from './component';
 
-export function createRouteComponent<TState extends f.IState, TData>(component: b.IBobrilComponent)
-    : (cursor: f.ICursor<TState>, data?: TData, children?: b.IBobrilChildren) => (data?: TData, children?: b.IBobrilChildren) => b.IBobrilNode {
-    return (c: f.ICursor<TState>, d?: TData, ch?: b.IBobrilChildren) =>
-        b.createVirtualComponent<TData>({
-            render(ctx: b.IBobrilCtx, me: b.IBobrilNode) {
-                me.children = cm.createComponent(component)(c, d);
-            }
-        });
+export interface IRouteData {
+    routeParams: b.Params
+}
+
+export interface IRouteContext<TState extends f.IState, TData extends IRouteData> extends cm.IContext<TState> {
+    data: TData
+    lastData: TData
+}
+
+export function createRouteComponent<TState extends f.IState, TData extends IRouteData>(component: b.IBobrilComponent)
+    : (cursor: f.ICursor<TState>) => (data?: TData, children?: b.IBobrilChildren) => b.IBobrilNode {
+    return (c: f.ICursor<TState>) =>
+        b.createDerivedComponent<TData>(
+            b.createComponent({
+                init(ctx: IRouteContext<TState, TData>) {
+                    ctx.state = f.getState(c);
+                    ctx.lastData = ctx.data;
+                },
+                shouldChange(ctx: IRouteContext<TState, TData>, me: b.IBobrilNode, oldMe: b.IBobrilCacheNode): boolean {
+                    if (ctx.forceShouldChange)
+                        return true;
+                    let currentState = f.getState(c);
+                    if (ctx.data === ctx.lastData && currentState === ctx.state)
+                        return false;
+                    ctx.state = currentState;
+                    ctx.lastData = ctx.data;
+                    return true;
+                }
+            }),
+            component)
 }
