5f3d8e9093fbf56fcd0c604fb86c72ff2405d0d4
k.steinmetz <karel.steinmetz@gmail.com>
2016-01-19 20:44:03 +0100

    Fixed mainPage header padding.

--- examples/todo/src/mainPage.ts
+++ examples/todo/src/mainPage.ts
@@ -2,6 +2,11 @@ import * as b from 'bobril';
 import * as gui from 'bobril-css-bootstrap';
 
 export let create = b.createComponent({
+    id: 'body-style',
+    postInitDom(ctx: b.IBobrilCtx, me: b.IBobrilCacheNode, element: HTMLElement) {
+        document.body.style.minHeight = '200px';
+        document.body.style.paddingTop = '70px';
+    },
     render(ctx: b.IBobrilCtx, me: b.IBobrilNode) {
         me.children = [
             gui.navBar({
