841464d7193bc1d3ac8193a19e0d660cfa98d017
k.steinmetz <karel.steinmetz@gmail.com>
2016-03-16 09:15:52 +0100

    Added export for data component.

--- index.ts
+++ index.ts
@@ -19,6 +19,7 @@ debugCallbackType
 
 export * from './src/component';
 export * from './src/routeComponent';
+export * from './src/dataComponent';
 
 export let bootstrap = (defaultState: IState, debugCallback: debugCallbackType = undefined, subStateSeparator: string = '.') => {
     funBootstrap(defaultState, () => b.invalidate(), (message, params) => { debugCallback && debugCallback(`bobflux -> ${message}`, params); });
