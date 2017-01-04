0f54a7d400b4b38401b387163f7ef8be65ea326e
k.steinmetz <karel.steinmetz@gmail.com>
2017-01-04 23:31:28 +0100

    Set localization to into example.

--- examples/app.ts
+++ examples/app.ts
@@ -1,10 +1,17 @@
 import * as b from "bobril";
+import * as g from "bobril-g11n";
 import * as f from "./src/flux";
 import * as s from "./src/states";
 import * as c from "./src/states.cursors";
 import * as mp from "./src/mainPage";
 import * as tdp from "./src/todos/todosPage";
 import * as wnp from "./src/whatNext/whatNextPage";
+
+g.initGlobalization({
+    defaultLocale: 'cs-CZ',
+    pathToTranslation(locale: string): string { return './translations/' + locale + '.js'; }
+});
+
 
 f.bootstrap(s.createDefaultApplicationState(), (message, params) => { });
 
@@ -16,3 +23,5 @@ b.routes(
         },
         mp.createRoutes())
 );
+
+g.setLocale('cs-CZ');
