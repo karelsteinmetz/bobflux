23e5b0604bc4cae2b383df3ef554d14199efcb7b
karel.steinmetz <karel.steinmetz@gmail.com>
2017-02-23 11:57:43 +0100

    Generated index.d.ts.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
-import { IState, debugCallbackType } from 'fun-model';
-export { ICursor, ICursorFactory, IState, IActionHandler, getState, setState, rootCursor, shallowCopy, IAction, createAction, createActions, IAsyncAction, createAsyncAction, debugCallbackType } from 'fun-model';
+import { IState, IBootstrapParams } from 'fun-model';
+export { IBootstrapParams, ICursor, ICursorFactory, IState, IActionHandler, getState, setState, rootCursor, shallowCopy, IAction, createAction, createActions, debugCallbackType } from 'fun-model';
 export * from './src/component';
 export * from './src/routeComponent';
 export * from './src/dataComponent';
-export declare let bootstrap: (defaultState: IState, debugCallback?: debugCallbackType, subStateSeparator?: string) => void;
+export declare const bootstrap: (defaultState: IState, params?: IBootstrapParams) => void;
 export declare const defaultStateName: string;
